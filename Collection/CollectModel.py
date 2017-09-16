d9f0fcc516a20e7572f39c519fc4a302af037d8c
youngbetter <young_better@sina.com>
2017-09-16 14:17:30 +0800

    #by young #update modify collection_model

--- Collection/CollectModel.py
+++ Collection/CollectModel.py
@@ -12,13 +12,12 @@ class CollModelHandler(object):
     # 用于登录时获取，作品集模型
     def get_collModel(self, uid):
         retdata = []
-        colls = get_db().query(UserCollection).filter(and_(
-                                             UserCollection.UCvalid == 1,
-                                             UserCollection.UCiscollection == 1)) \
+        colls = get_db().query(UserCollection).filter(and_(UserCollection.UCvalid == 1,
+                                                           UserCollection.UCiscollection == 1)) \
             .order_by(desc(UserCollection.UCcreateT)).limit(10).all()
         collimgurl = []
         for coll in colls:
-            imgs = get_db().query(UserCollectionimg).filter(UserCollectionimg.UCIuser == UserCollection.UCid).all()
+            imgs = get_db().query(UserCollectionimg).filter(UserCollectionimg.UCIuser == coll.UCid).all()
             for img in imgs:
                 collimgurl.append(img.UCIurl)
             self.response_one(coll, collimgurl, retdata)
