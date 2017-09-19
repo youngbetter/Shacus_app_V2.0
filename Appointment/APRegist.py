8d420a31bb89f11a76b17461524d2956661641ed
Jerry <1192358340@qq.com>
2017-09-19 09:29:52 +0800

    用户不能报名自己发布的约拍  by Jerry

--- Appointment/APRegist.py
+++ Appointment/APRegist.py
@@ -10,7 +10,7 @@ from Userinfo.Ufuncs import Ufuncs
 '''
 
 
-class APRegistHandler(BaseHandler):   # 报名约拍
+class APRegistHandler(BaseHandler):  # 报名约拍
 
     retjson = {'code': '', 'contents': ''}
 
@@ -27,15 +27,20 @@ class APRegistHandler(BaseHandler):   # 报名约拍
         ufunc = Ufuncs()
 
         if ufunc.judge_user_valid(u_id, u_auth_key):  # 用户认证成功
-            if ap_type == '80003':   # 报名约拍
+            if ap_type == '80003':  # 报名约拍
                 # ap_message = self.get_argument('message')
                 try:
                     appointment = self.db.query(Appointment).filter(Appointment.APid == ap_id).one()
+                    if int(u_id) == appointment.APsponsorid:
+                        self.retjson['contents'] = '不能报名自己的约拍'
+                        self.write(json.dumps(self.retjson, ensure_ascii=False, indent=2))
+                        return
+                        self.retjson['code'] = '800040'
                     if appointment.APvalid == 0:
                         self.retjson['contents'] = '该约拍已删除'
                         self.retjson['code'] = '800035'
                     else:
-                        if appointment.APstatus == 0:    # 还在报名期
+                        if appointment.APstatus == 0:  # 还在报名期
                             try:
                                 exist = self.db.query(AppointEntry). \
                                     filter(AppointEntry.AEregisterID == u_id, AppointEntry.AEapid == ap_id,
@@ -72,14 +77,14 @@ class APRegistHandler(BaseHandler):   # 报名约拍
                                     print e
                                     self.db_error()
                         else:
-                             self.retjson['contents'] = '该约拍报名期已过'
-                             self.retjson['code'] = '800036'
+                            self.retjson['contents'] = '该约拍报名期已过'
+                            self.retjson['code'] = '800036'
                 except Exception, e:
                     print e
                     self.retjson['contents'] = '数据库查询失败'
                     self.retjson['code'] = '800033'
 
-            elif ap_type == '80004':   # 用户取消报名
+            elif ap_type == '80004':  # 用户取消报名
                 try:
                     appointment = self.db.query(Appointment).filter(Appointment.APid == ap_id).one()
                     if appointment.APvalid == 0:
@@ -88,7 +93,7 @@ class APRegistHandler(BaseHandler):   # 报名约拍
                     else:
                         if appointment.APstatus == 0:
                             try:
-                                entry = self.db.query(AppointEntry).filter(AppointEntry.AEapid == ap_id,\
+                                entry = self.db.query(AppointEntry).filter(AppointEntry.AEapid == ap_id, \
                                                                            AppointEntry.AEregisterID == u_id).one()
                                 if entry.AEvalid == 0:
                                     self.retjson['contents'] = '该约拍已取消'
@@ -109,8 +114,8 @@ class APRegistHandler(BaseHandler):   # 报名约拍
                                 self.retjson['contents'] = '数据库查询失败'
                                 self.retjson['code'] = '800033'
                         else:
-                             self.retjson['contents'] = '该约拍已过报名期，不能取消'
-                             self.retjson['code'] = '800039'
+                            self.retjson['contents'] = '该约拍已过报名期，不能取消'
+                            self.retjson['code'] = '800039'
                 except Exception, e:
                     print e
                     self.retjson['contents'] = '数据库查询出错'
@@ -119,5 +124,5 @@ class APRegistHandler(BaseHandler):   # 报名约拍
         else:
             self.retjson['code'] = '10272'
             self.retjson['contents'] = r'用户认证失败'
-     
+
         self.write(json.dumps(self.retjson, ensure_ascii=False, indent=2))
